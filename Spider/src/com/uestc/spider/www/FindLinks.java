7a443fb4fe3a66e7a946386456fcecd9dafc80dc
yincunvhun <49662581@qq.com>
2014-11-13 19:44:09 +0800

    1.国内新闻主题links 2.国内新闻内容links

--- Spider/src/com/uestc/spider/www/FindLinks.java
+++ Spider/src/com/uestc/spider/www/FindLinks.java
@@ -7,29 +7,29 @@ import java.util.Queue;
  * */
 public interface FindLinks {
 
-	public Queue<String> findThemeLinks(String themeLink) ; //获取主题链接
+	public Queue<String> findThemeLinks(String themeLink ,String themeLinkReg) ; //获取主题链接
 	
-	public Queue<String> findContentLinks(String themeLink) ;  //获取内容链接
+	public Queue<String> findContentLinks(Queue<String> themeLink ,String ContentLinkReg) ;  //获取内容链接
 	
-	public String findNewTitle(String html) ; //获取新闻标题
+	public String findNewsTitle(String html) ; //获取新闻标题
 	
-	public String findNewOriginalTite(String html) ; //获取新闻原始标题
+	public String findNewsOriginalTite(String html) ; //获取新闻原始标题
 	
-	public String findNewContent(String html) ;    //获取新闻内容
+	public String findNewsContent(String html) ;    //获取新闻内容
 	
-	public String findNewImages(String html);     //获取新闻图片
+	public String findNewsImages(String html);     //获取新闻图片
 	 
-	public String findNewTime(String html) ;        //获取新闻发布时间
+	public String findNewsTime(String html) ;        //获取新闻发布时间
 	
-	public String findNewSource() ;           //新闻来源
+	public String findNewsSource() ;           //新闻来源
 	
-	public String findNewOriginalSource() ;     //新闻具体来源
+	public String findNewsOriginalSource() ;     //新闻具体来源
 	 
-	public String findNewCategroy(String html) ;  //新闻版面属性
+	public String findNewsCategroy(String html) ;  //新闻版面属性
 	
-	public String findNewOriginalCategroy(String html); //新闻具体版面属性
+	public String findNewsOriginalCategroy(String html); //新闻具体版面属性
 	
-	public String findNewComment(String url) ;         //获取新闻评论
+	public String findNewsComment(String url) ;         //获取新闻评论
 	
 	public void handle();                              //把所有标签加入数据库
 	
--- Spider/src/com/uestc/spider/www/GetNewsFromPortals.java
+++ Spider/src/com/uestc/spider/www/GetNewsFromPortals.java
@@ -1,7 +1,20 @@
 package com.uestc.spider.www;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.htmlparser.Node;
+import org.htmlparser.NodeFilter;
+import org.htmlparser.Parser;
+import org.htmlparser.tags.LinkTag;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
 
 /*
  * 对每一个门户网站顶一个特有的类
@@ -13,103 +26,212 @@ import java.util.Queue;
  * */
 class NETEASENews implements FindLinks{
 	
+	private String ENCODE ;
+	
+	public NETEASENews(String encode){
+		this.ENCODE = encode ;
+	}
+	
 	//保存获取到的主题links
 	public Queue<String> newsThemeLinks = new LinkedList<String>() ;
 	
 	//保存获取到的新闻links
 	public Queue<String> newsContentLinks = new LinkedList<String>() ;
 	
-	
 	//保存已经访问的新闻links 以免新闻重复
 	public Queue<String> linksVisited = new LinkedList<String>() ;
+	
+	//新闻主题links的正则表达式
+	String newsThemeLinksReg ; //= "http://news.163.com/special/0001124J/guoneinews_[0-9]{1,2}.html#headList";
+			
+	//新闻内容links的正则表达式
+	String newsContentLinksReg ; //= "http://news.163.com/[0-9]{2}/[0-9]{4}/[0-9]{2}/(.*?).html#f=dlist";
 	
 	//新闻主题link
 	private String theme ;
 	
-	public NETEASENews(String theme){
-		
-		this.theme = theme ;
-	}
+//	public NETEASENews(String theme){
+//		
+//		this.theme = theme ;
+//	}
 	//获取国内新闻
 	public void getGuoNeiNews(){
 		
+		//国内新闻 首页链接
+		theme = "http://news.163.com/domestic/";
+		
+		int state ;
+		try{
+			HttpURLConnection httpUrlConnection = (HttpURLConnection) new URL(theme).openConnection(); //创建连接
+			state = httpUrlConnection.getResponseCode();
+			httpUrlConnection.disconnect();
+		}catch (MalformedURLException e) {
+//          e.printStackTrace();
+			System.out.println("网络慢，已经无法正常链接，无法获取新闻");
+			return;
+		} catch (IOException e) {
+          // TODO Auto-generated catch block
+//          e.printStackTrace();
+			System.out.println("网络超级慢，已经无法正常链接，无法获取新闻");
+			return ;
+      }
+		if(state != 200 && state != 201){
+			return;
+		}
+		
 		//新闻主题links的正则表达式
-		String newsThemeLinksReg = "http://news.163.com/special/0001124J/guoneinews_[0-9]{1,2}.html#headList";
+		newsThemeLinksReg = "http://news.163.com/special/0001124J/guoneinews_[0-9]{1,2}.html#headList";
 		
 		//新闻内容links的正则表达式
-		String newsContentLinksReg = "http://news.163.com/[0-9]{2}/[0-9]{4}/[0-9]{2}/(.*?).html#f=dlist";
+		newsContentLinksReg = "http://news.163.com/[0-9]{2}/[0-9]{4}/[0-9]{2}/(.*?).html#f=dlist";
 		
 		//保存国内新闻主题links
 		Queue<String> guoNeiTheme = new LinkedList<String>();
-		guoNeiTheme.offer("http://news.163.com/domestic/");
-		for(int i = 2 ; i < 11 ; i++){
-			if(i < 10)
-				guoNeiTheme.offer("http://news.163.com/special/0001124J/guoneinews_0"+i+".html#headList");
-			else
-				guoNeiTheme.offer("http://news.163.com/special/0001124J/guoneinews_"+i+".html#headList");
+		guoNeiTheme = findThemeLinks(theme,newsThemeLinksReg);
+		System.out.println(guoNeiTheme);
+		
+		//获取国内新闻内容links
+		Queue<String>guoNeiNews = new LinkedList<String>();
+		guoNeiNews = findContentLinks(guoNeiTheme,newsContentLinksReg);
+		
+
+	}
+	@Override
+	public Queue<String> findThemeLinks(String themeLink ,String themeLinkReg) {
+		
+		// TODO Auto-generated method stub
+		Queue<String> themelinks = new LinkedList<String>();
+		Pattern newsThemeLink = Pattern.compile(themeLinkReg);
+		themelinks.offer(themeLink);
+		
+		try {
+				Parser parser = new Parser(themeLink);
+				parser.setEncoding(ENCODE);
+				NodeList nodeList = parser.extractAllNodesThatMatch(new NodeFilter(){
+					public boolean accept(Node node)
+					{
+						if (node instanceof LinkTag)// 标记
+							return true;
+						return false;
+					}
 			
+				});
+				
+				for (int i = 0; i < nodeList.size(); i++)
+				{
+				
+					LinkTag n = (LinkTag) nodeList.elementAt(i);
+//		        	System.out.print(n.getStringText() + "==>> ");
+//		       	 	System.out.println(n.extractLink());
+					//新闻主题
+					Matcher themeMatcher = newsThemeLink.matcher(n.extractLink());
+					if(themeMatcher.find()){
+						
+		        		themelinks.offer(n.extractLink());
+		        	}
+		        			
+		        	
+
+				}
+			}catch(ParserException e){
+				return null;
+			}catch(Exception e){
+				return null;
+			}
+		return themelinks ;
+	}
+
+	public Queue<String> findContentLinks(Queue<String> themeLink ,String contentLinkReg) {
+		// TODO Auto-generated method stub
+		Queue<String> contentlinks = new LinkedList<String>(); // 临时征用
+		
+		Pattern newsContent = Pattern.compile(contentLinkReg);
+		while(!themeLink.isEmpty()){
+			String buf = themeLink.poll();
+			try {
+				Parser parser = new Parser(buf);
+				parser.setEncoding(ENCODE);
+				NodeList nodeList = parser.extractAllNodesThatMatch(new NodeFilter(){
+					public boolean accept(Node node)
+					{
+						if (node instanceof LinkTag)// 标记
+							return true;
+						return false;
+					}
+		
+				});
+			
+				for (int i = 0; i < nodeList.size(); i++)
+				{
+			
+					LinkTag n = (LinkTag) nodeList.elementAt(i);
+//	        	System.out.print(n.getStringText() + "==>> ");
+//	       	 	System.out.println(n.extractLink());
+					//新闻主题
+					Matcher themeMatcher = newsContent.matcher(n.extractLink());
+					if(themeMatcher.find()){
+					
+						if(!contentlinks.contains(n.extractLink()))
+							contentlinks.offer(n.extractLink());
+					}
+					
+				}
+			}catch(ParserException e){
+				return null;
+			}catch(Exception e){
+				return null;
+			}		
 		}
-//		System.out.println(guoNeiTheme);
-
+		System.out.println(contentlinks);
+		return contentlinks;
 	}
 	@Override
-	public Queue<String> findThemeLinks(String themeLink) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Queue<String> findContentLinks(String themeLink) {
+	public String findNewsTitle(String html) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	@Override
-	public String findNewTitle(String html) {
+	public String findNewsOriginalTite(String html) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	@Override
-	public String findNewOriginalTite(String html) {
+	public String findNewsContent(String html) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	@Override
-	public String findNewContent(String html) {
+	public String findNewsImages(String html) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	@Override
-	public String findNewImages(String html) {
+	public String findNewsTime(String html) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	@Override
-	public String findNewTime(String html) {
+	public String findNewsSource() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	@Override
-	public String findNewSource() {
+	public String findNewsOriginalSource() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	@Override
-	public String findNewOriginalSource() {
+	public String findNewsCategroy(String html) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	@Override
-	public String findNewCategroy(String html) {
+	public String findNewsOriginalCategroy(String html) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	@Override
-	public String findNewOriginalCategroy(String html) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public String findNewComment(String url) {
+	public String findNewsComment(String url) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -125,73 +247,72 @@ class NETEASENews implements FindLinks{
 class SINANews implements FindLinks{
 
 	@Override
-	public Queue<String> findThemeLinks(String themeLink) {
+	public Queue<String> findThemeLinks(String themeLink , String themeLinkReg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Queue<String> findContentLinks(Queue<String> themeLink,String contentLinkReg) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Queue<String> findContentLinks(String themeLink) {
+	public String findNewsTitle(String html) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String findNewTitle(String html) {
+	public String findNewsOriginalTite(String html) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String findNewOriginalTite(String html) {
+	public String findNewsContent(String html) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String findNewContent(String html) {
+	public String findNewsImages(String html) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String findNewImages(String html) {
+	public String findNewsTime(String html) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String findNewTime(String html) {
+	public String findNewsSource() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String findNewSource() {
+	public String findNewsOriginalSource() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String findNewOriginalSource() {
+	public String findNewsCategroy(String html) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String findNewCategroy(String html) {
+	public String findNewsOriginalCategroy(String html) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String findNewOriginalCategroy(String html) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String findNewComment(String url) {
+	public String findNewsComment(String url) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -205,9 +326,10 @@ class SINANews implements FindLinks{
 }
 
 public class GetNewsFromPortals {
+	
 
 	public static void main(String[] args){
-		NETEASENews test = new NETEASENews("");
+		NETEASENews test = new NETEASENews("utf-8");
 		test.getGuoNeiNews();
 	}
 	
